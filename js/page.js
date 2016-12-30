82afc10b4fa8535a05d1efabce8318acf0e30634
Trina Lu <tri613@gmail.com>
2016-12-31 01:27:21 +0800

    add small hearts

--- js/page.js
+++ js/page.js
@@ -5,9 +5,28 @@
 	const fadeInFirst = document.querySelectorAll('.animated[data-order="1"]');
 	const fadeInSecond = document.querySelectorAll('.animated[data-order="2"]');
 	const openBtn = document.querySelectorAll('.open-btn');
+	const hearts = document.querySelectorAll('.sm-heart');
+
+	function flyHearts(e) {
+		let xs = [];
+		let delays = [];
+		if (e.animationName == "showHeart") {
+			// hearts.forEach((heart, i) => {
+			// 	const x = (Math.floor(Math.random() * 20) - 10) * 20;
+			// 	const delay = Math.round(Math.random() * 100) / 100;
+			// 	xs.push(x);
+			// 	delays.push(delay);
+			// 	heart.style.transitionDelay = `${delay}s`;
+			// 	heart.style.webkitTransitionDelay = `${delay}s`;
+			// 	heart.style.transform = `translate(${x}%, -100vh)`;
+			// });
+			// console.log(xs, delays);
+			hearts.forEach(heart =>  heart.classList.add('active'));
+		}
+	}
 
 	function animationEndHandler (e) {
-		if (e.animationName == "scaleToFullSreen") {
+		if (e.animationName == "fadeOutHeart") {
 			renderMainPage();
 			setTimeout(renderMainSecond, 500);
 		}
@@ -34,6 +53,7 @@
 		content.classList.toggle('open');
 	}
 
+	heart.addEventListener("animationstart", flyHearts);
 	heart.addEventListener("animationend", animationEndHandler);
 	openBtn.forEach(btn => btn.addEventListener('click', toggleContent));
 })();
